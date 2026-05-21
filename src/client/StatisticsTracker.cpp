7d096997bf98b3169211b37b7de5be3f652fa0da
Kyuhyun <kyuhyunp@umich.edu>
2026-05-20 18:12:31 -0700

    network client

--- src/client/StatisticsTracker.cpp
+++ src/client/StatisticsTracker.cpp
@@ -18,14 +18,9 @@ void StatisticsTracker::update(sf::Time dt)
 
     if (mUpdateTime >= sf::seconds(1.0f)) {
         float fps = static_cast<float>(mNumFrames) / mUpdateTime.asSeconds();
+        auto timePerUpdateUs = mUpdateTime.asMicroseconds() / mNumFrames;
 
-        std::ostringstream stream;
-        stream << std::fixed << std::setprecision(2);
-        stream << "Frames / Second = " << fps << "\n";
-        stream << "Time / Update = "
-               << (mUpdateTime.asMicroseconds() / mNumFrames) << "us";
-
-        spdlog::info(stream.str());
+        spdlog::info("Frames / Second = {:.2f}\nTime / Update = {}us", fps, timePerUpdateUs);
 
         // reset counters
         mUpdateTime -= sf::seconds(1.0f);
