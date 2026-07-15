2cc86ba299a0d175f7faf6fb6a4df6715712863e
Kyuhyun <kyuhyunp@umich.edu>
2026-07-14 20:48:52 -0700

    username state

--- src/client/States/UsernameState.cpp
+++ src/client/States/UsernameState.cpp
@@ -17,14 +17,8 @@ UsernameState::UsernameState(StateStack& stack, Context context)
     sf::Vector2f windowSize = context.window->getView().getSize();
     const sf::Font& font = context.fonts->get(Fonts::ID::Sansation);
 
-    // Title Label
-    auto title = std::make_unique<GUI::Label>("My username is", font);
-    sf::FloatRect titleBounds = title->getGlobalBounds();
-    title->setOrigin(titleBounds.size / 2.f);
-    title->setPosition({ windowSize.x / 2.f, windowSize.y / 2.f - 80.f });
-
     // Username TextBox
-    auto username = std::make_unique<GUI::TextBox>(font, sf::Vector2f({ 200.f, 50.f }));
+    auto username = std::make_unique<GUI::TextBox>("Enter Username", font, sf::Vector2f({ 200.f, 50.f }));
     sf::FloatRect usernameBounds = username->getGlobalBounds();
     username->setOrigin(usernameBounds.size / 2.f);
     username->setPosition({ windowSize.x / 2.f, windowSize.y / 2.f - 25.f });
@@ -59,7 +53,6 @@ UsernameState::UsernameState(StateStack& stack, Context context)
         requestStackPush(States::ID::Title);
     });
 
-    mGUIContainer.pack(std::move(title));
     mGUIContainer.pack(std::move(username));
     mGUIContainer.pack(std::move(joinGame));
     mGUIContainer.pack(std::move(leave));
@@ -72,24 +65,19 @@ void UsernameState::draw()
 
 bool UsernameState::update(sf::Time dt)
 {
+	mGUIContainer.update(dt);
     return false;
 }
 
 bool UsernameState::handleEvent(const sf::Event& event)
 {
-    const auto window = getContext().window;
+    auto worldMousePos = getWorldMousePosition(event);
+    mGUIContainer.handleEvent(event, worldMousePos); 
 
-    std::optional<sf::Vector2f> worldMousePos = std::nullopt;
-    
-    if (auto mouseMoved = event.getIf<sf::Event::MouseMoved>())
+    if (event.getIf<sf::Event::MouseMoved>() && worldMousePos.has_value())
     {
-        worldMousePos = window->mapPixelToCoords(mouseMoved->position);
-    }
-    else if (auto mousePressed = event.getIf<sf::Event::MouseButtonPressed>())
-    {
-        worldMousePos = window->mapPixelToCoords(mousePressed->position);
+        setMouseCursor(mGUIContainer.getMouseCursorType(worldMousePos.value()).value_or(sf::Cursor::Type::Arrow));
     }
 
-    mGUIContainer.handleEvent(event, worldMousePos);
     return false;
 }
